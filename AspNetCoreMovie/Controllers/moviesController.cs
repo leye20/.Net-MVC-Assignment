a255669acb93b5816ffbcda8fbe8a09f25d0a916
leye20 <leyeagbo@gmail.com>
2020-05-15 01:07:48 -0600

    added controller and model class

---
+++ AspNetCoreMovie/Controllers/moviesController.cs
@@ -0,0 +1,16 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMovie.Controllers
+{
+    public class moviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}*/
