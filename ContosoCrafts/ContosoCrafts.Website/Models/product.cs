7f7dc7ac960646a65f680957891fe57221ef3dda
leye20 <leyeagbo@gmail.com>
2020-05-19 17:48:26 -0600

    first commit

---
+++ ContosoCrafts/ContosoCrafts.Website/Models/product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.Website.Models
+{
+    public class Product
+    {
+        public string id { get; set; }
+        public string Maker { get; set; }
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
