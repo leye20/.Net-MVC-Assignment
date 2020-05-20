afeeacc7d9ab36a356888d231778f4df30e03c1a
leye20 <leyeagbo@gmail.com>
2020-05-19 18:53:55 -0600

    output/displayed data

--- ContosoCrafts/ContosoCrafts.Website/Pages/Index.cshtml.cs
+++ ContosoCrafts/ContosoCrafts.Website/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.Website.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,15 +13,19 @@ namespace ContosoCrafts.Website.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService) // Adding services so any that requests for a services gets one.
         {
             _logger = logger;
+            ProductService = productService;
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
